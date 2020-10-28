daa1eb803c50c88d93d8b17694ea710cc09e2d37
ANSR <andreas.schmider98@gmail.com>
2020-10-28 15:56:13 +0100

    MainActivity simplified

--- app/src/main/java/com/example/spybot/MainActivity.java
+++ app/src/main/java/com/example/spybot/MainActivity.java
@@ -25,8 +25,7 @@ public class MainActivity extends AppCompatActivity {
         layout.setOrientation(LinearLayout.VERTICAL);
         layout.setBackgroundResource(R.drawable.background);
 
-        // 76 x:7 y:6
-        int check, name;
+
         Random random = new Random();
 
 
@@ -37,32 +36,11 @@ public class MainActivity extends AppCompatActivity {
                             LinearLayout.LayoutParams.WRAP_CONTENT));
 
             for (int x = 0; x < width; x++) {
-                createButton2(row, y * height + x, View.VISIBLE);
+                createButton2(row, y * width + x, View.VISIBLE);
             }
             layout.addView(row);
         }
 
-        /*
-        for (int y = 0; y < width; y++) {
-            LinearLayout row = new LinearLayout(this);
-            row.setLayoutParams(new LinearLayout.LayoutParams
-                    (LinearLayout.LayoutParams.WRAP_CONTENT,
-                            LinearLayout.LayoutParams.WRAP_CONTENT));
-            for (int x = 0; x < height; x++) {
-
-                check = random.nextInt(101);
-                if (check % 5 == 0) {
-                    //createButton("O", row, y, x, false);
-                    createButton2(row, y * height + x, View.INVISIBLE);
-                } else {
-                    //name = y * xRange + x;
-                    //createButton(Integer.toString(name), row, y, x, true);
-                    createButton2(row, y * height + x, View.VISIBLE);
-                }
-
-            }
-            layout.addView(row);
-        }*/
         setContentView(layout);
         //resetButtons();
     }
@@ -78,61 +56,19 @@ public class MainActivity extends AppCompatActivity {
 
         btnTag.setText(Integer.toString(id));
         btnTag.setId(id);
-        btnTag.setWidth(40);
-        btnTag.setHeight(40);
 
         btnTag.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //resetButtons();
                 Button clickedButton = (Button) findViewById(v.getId());
                 clickedButton.setBackgroundColor(0xFF00FF00);
-                //OnClick(v.getId());
+                OnClick(v.getId());
             }
         });
-
 
         btnTag.setVisibility(visibility);
-
         layout.addView(btnTag);
     }
-
-    void createButton(String name, LinearLayout layout, int y, int x, boolean visible) {
-        Button btnTag = new Button(this);
-
-        DisplayMetrics dm = new DisplayMetrics();
-        this.getWindow().getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int width = dm.widthPixels;
-
-
-        btnTag.setLayoutParams(new LinearLayout.LayoutParams(width / 20, width / 20));
-
-
-        btnTag.setText(name);
-        btnTag.setId(x * 10 + y);
-        btnTag.setWidth(40);
-        btnTag.setHeight(40);
-        btnTag.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //resetButtons();
-                Button clickedButton = (Button) findViewById(v.getId());
-                clickedButton.setBackgroundColor(0xFF00FF00);
-                //OnClick(v.getId());
-            }
-        });
-        if (visible) {
-            btnTag.setVisibility(View.VISIBLE);
-        } else {
-            btnTag.setVisibility(View.INVISIBLE);
-        }
-
-        layout.addView(btnTag);
-
-
-    }
-
-
 
     void OnClick(int id) {
         Random random = new Random();
@@ -140,31 +76,15 @@ public class MainActivity extends AppCompatActivity {
 
         Button button = null;
         while (button == null) {
-            int x = random.nextInt(16) + 1;
-            int y = random.nextInt(8) + 1;
-            button = (Button) findViewById(x * 10 + y);
+            int x = random.nextInt(width) + 1;
+            int y = random.nextInt(height) + 1;
+            button = (Button) findViewById(y * width + x);
             if (button != null) {
                 if (button.getVisibility() == View.INVISIBLE) {
                     button = null;
                 } else {
                     button.setBackgroundColor(0xFF00FF00);
-                    //GetDirection(id,x*10+y);
                 }
-            }
-
-
-        }
-
-
-    }
-
-
-
-    void resetButtons() {
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                Button button = (Button) findViewById(y * width + x);
-                button.setBackgroundColor(0xcccccc);
             }
         }
     }
