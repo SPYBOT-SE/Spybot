9bd62484fddd204ea9fb1946dc17b733ddb75ab6
ANSR <andreas.schmider98@gmail.com>
2020-11-08 15:10:23 +0100

    AdjacencyList Attribute added to Board Graph will be created in constructor and uses data from the Field array board

--- app/src/main/java/com/level/Board.java
+++ app/src/main/java/com/level/Board.java
@@ -1,19 +1,89 @@
 package com.level;
 
+import com.model.AdjacencyList;
+
 public class Board {
-    private int sizeX;
-    private int sizeY;
+    private final int sizeX;
+    private final int sizeY;
 
     Field[][] board;
+    AdjacencyList<Field> graph;
 
-    Board(int sizeX, int sizeY ) {
+    public Board(int sizeX, int sizeY ) {
         this.sizeX = sizeX;
         this.sizeY = sizeY;
 
-        this.board = new Field[sizeX][sizeY];
+        board = new Field[sizeX][sizeY];
+        initBoard();
+        initGraph();
     }
 
 
+    private void initBoard() {
+        for (int y = 0; y < sizeY; y++) {
+            for (int x = 0; x < sizeX; x++) {
+                board[x][y] = new Field();
+            }
+        }
+
+
+        board[1][1].setStatus(true);
+        board[1][0].setStatus(true);
+        board[1][2].setStatus(true);
+        board[0][1].setStatus(true);
+        board[2][1].setStatus(true);
+
+        board[0][0].setStatus(true);
+        board[15][7].setStatus(true);
+    }
+
+    private void initGraph() {
+        graph = new AdjacencyList<>();
+
+        int top, bottom, left, right;
+        Field middle, neighbour;
+
+        for (int y = 0; y < sizeY; y++) {
+            for (int x = 0; x < sizeX; x++) {
+                middle = board[x][y];
+                if (middle.getStatus()) {
+                    graph.addVertex(middle);
+
+                    top = y - 1;
+                    if (top >= 0 && top < sizeY) {
+                        neighbour = board[x][top];
+                        if (neighbour.getStatus()) {
+                            graph.addEdge(middle, neighbour);
+                        }
+                    }
+
+                    bottom = y + 1;
+                    if (bottom >= 0 && bottom < sizeY) {
+                        neighbour = board[x][bottom];
+                        if (neighbour.getStatus()) {
+                            graph.addEdge(middle, neighbour);
+                        }
+                    }
+
+                    left = x - 1;
+                    if (left >= 0 && left < sizeY) {
+                        neighbour = board[left][y];
+                        if (neighbour.getStatus()) {
+                            graph.addEdge(middle, neighbour);
+                        }
+                    }
+
+                    right = x + 1;
+                    if (right >= 0 && right < sizeY) {
+                        neighbour = board[right][y];
+                        if (neighbour.getStatus()) {
+                            graph.addEdge(middle, neighbour);
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 
 }
